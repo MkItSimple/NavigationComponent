e12b2fec26e1c3fe14938cdbeb490c7b13db81e9
MkItSimple <choanania1@gmail.com>
2020-03-03 14:07:49 +0800

    Branch01_Working

--- app/src/main/java/com/example/navigationcomponent/ChooseRecipientFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/ChooseRecipientFragment.kt
@@ -2,15 +2,24 @@ package com.example.navigationcomponent
 
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_choose_recipient.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipientFragment : Fragment() {
+class ChooseRecipientFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +30,29 @@ class ChooseRecipientFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.next_btn -> {
+                if(!TextUtils.isEmpty(input_recipient.text.toString())){
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController.navigate(
+                        R.id.action_chooseRecipientFragment_to_specifyAmountFragment,
+                        bundle
+                    )
+                }
+                else{
+                    Toast.makeText(activity, "Enter a name", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
+    }
 }
